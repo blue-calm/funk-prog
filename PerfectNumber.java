09d0dd5a67125c2b4a12b51906050d62ad640e92
blue-calm <55148016+blue-calm@users.noreply.github.com>
2019-09-19 18:46:49 +0300

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,42 +1,49 @@
-//Laboratorijas darbs 1-2
+//Laboratorijas darbs 1-1
 package imperative;
+import java.util.Scanner;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class PerfectNumber {
-        
+
         public enum STATE {
-		 
+
 		ABUNDANT,DEFICIENT,PERFECT;
 		}
-	
-        
+
+
        public static STATE process(int i){    	 
           Set<Integer> h = new LinkedHashSet<Integer>();
           h = divisors(i);
-          Integer integerSum = h.stream().mapToInt(Integer::intValue).sum();
-	        System.out.println("summation: " + integerSum/2);
-         return  integerSum/2 == i ? STATE.PERFECT:((integerSum/2 <i) ? STATE.DEFICIENT:STATE.ABUNDANT);
-         // return STATE.PERFECT;
+          int sum = 0;
+          Iterator<Integer> iter = h.iterator();
+          while (iter.hasNext()) {
+        	  sum+= (int)iter.next();
+          };
+          sum-=i;
+          if (sum <i) return STATE.DEFICIENT;
+          else if (sum == i) return STATE.PERFECT;
+          else return STATE.ABUNDANT;
         }
-       
+
       static Set <Integer> divisors(int x){    	   
-    	   Set<Integer> s1 = IntStream.rangeClosed(1, (int)Math.sqrt(x)) 
-  		.filter(num -> x % num == 0) 
-           .boxed()
-           .collect(Collectors.toSet());
-  		Set<Integer> s2 = s1.stream()
-  			.map(num -> x / num)
-  			.collect(Collectors.toSet());
-  		 s1.addAll(s2);
- 		 System.out.println(s1);
-       		return s1;
+    	   Set<Integer> s = new LinkedHashSet<Integer>(); 
+   			for (int k=1;k<=x;k++) {
+   			if (x%k==0) {
+   				s.add(k);		
+   				}
+   		}
+   			System.out.println("The Set: " + s); 
+       		return s;
        }
        public static void main (String[] args) {
-    	   int checkNum=6;
+    	   int checkNum=28;
     	   STATE state = process(checkNum);
     	   System.out.println(state);
        }
-       
+
 }
